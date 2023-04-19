f6a4550ba48bd121b7231937d9dff5cce6c14515
Luislvrz <u201922177@upc.edu.pe>
2023-04-19 02:41:17 -0500

    add boleta

--- boletaCrud/src/app/app.module.ts
+++ boletaCrud/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoletaComponent } from './component/boleta/boleta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoletaComponent
   ],
   imports: [
     BrowserModule,
